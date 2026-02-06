09cbcf00c496c8c4b7cfb53365eee952c2d08233
Shrasti Tiwari <shrastiwari13@gmail.com>
2026-02-06 21:05:23 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/twitter-clone/',   
   plugins: [react()],
-  base: '/twitter-clone/',
 })
