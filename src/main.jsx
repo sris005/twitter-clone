4dc42cadffce6ef2236b659bd9aac5eb53205440
Shrasti Tiwari <shrastiwari13@gmail.com>
2026-02-06 21:00:59 +0530

    Final fix for GitHub Pages blank screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
-import { HashRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
-<HashRouter>
-  <App />
-</HashRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+)
