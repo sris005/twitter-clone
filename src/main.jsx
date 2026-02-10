246f81e43c8fbcd7b5bb550347e7b55a2a5a34ad
Shrasti Tiwari <shrastiwari13@gmail.com>
2026-02-10 17:47:49 +0530

    Fix main.jsx with HashRouter (final)

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
